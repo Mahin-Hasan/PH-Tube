70118dd557998a77c7048168569e5847d28fc7e5
Mahin Hasan <mahinmtrs@gmail.com>
2023-10-14 21:16:16 +0600

    added categories dynamically

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const loadCategory = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await res.json();
+    console.log(data.data);
+    displayCategories(data);
+}
+
+const displayCategories =(categories)=>{
+    // console.log(category.data);
+    const categoryContainer = document.getElementById('categories-container');
+    const allData = categories.data;
+    allData.forEach((category)=>{
+        // console.log(category.category);
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <a class="btn btn-sm mr-2">${category.category}</a>
+        `;
+        categoryContainer.appendChild(div);
+    })
+}
+
+    loadCategory();
